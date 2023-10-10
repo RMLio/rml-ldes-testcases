fdf22e558d174b33d2f7c0846363a537d622c331
Dylan Van Assche <dylan.vanassche@ugent.be>
2023-10-10 18:49:45 +0200

    docs: adjust webversion of RML-LDES test cases
    Based on RML test cases.

--- docs/_data/testcases.js
+++ docs/_data/testcases.js
@@ -30,16 +30,12 @@ async function getTestCases(path) {
           ]
         ];
         <http://purl.org/dc/terms/identifier> ?id;
-        <http://www.w3.org/2006/03/test-description#expectedResults> ?expectedResult.
-        
-        OPTIONAL { ?expectedResult <http://www.w3.org/ns/dcat#distribution> [
-          <http://www.w3.org/ns/dcat#downloadUrl> ?output
-        ]} .
-        
-        OPTIONAL { ?s <http://www.w3.org/2006/03/test-description#specificationReference> ?specRef } .
+        <http://www.w3.org/2006/03/test-description#specificationReference> ?specRef;
+    .
   }`,
     { sources: [{ type: 'rdfjsSource', value: rdfjsSource }] });
   const bindings = await bindingsStream.toArray();
+  let processed = [];
   for (let index = 0; index < bindings.length; index++) {
     const data = bindings[index];
     const id = data.get('id').value;
@@ -47,37 +43,54 @@ async function getTestCases(path) {
     if (id.indexOf('SPARQL') === -1) {
       console.log(`${id}: started.`);
 
-      let output = null;
-      let outputStr = null;
-      if (data.has('output')) {
-        output = data.get('output').value;
-        outputStr = await fs.promises.readFile(output.replace('https://raw.githubusercontent.com/RMLio/rml-test-cases/master', '..'), 'utf-8')
-        outputStr = outputStr.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        console.log(`${id}: output is read from file`);
-      }
+      let outputBase = null;
+      let outputBaseStr = null;
+      outputBase = "../test-cases/" + id + "/output-base.nq";
+      outputBaseStr = await fs.promises.readFile(outputBase, 'utf-8')
+      outputBaseStr = outputBaseStr.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      console.log(`${id}: output base is read from file`);
 
-      const rules = data.get('rules').value;
-      let rulesStr = await fs.promises.readFile(rules.replace('https://raw.githubusercontent.com/RMLio/rml-test-cases/master', '..'), 'utf-8');
-      rulesStr = rulesStr.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-      console.log(`${id}: rules are read from file`);
+      let outputChange = null;
+      let outputChangeStr = null;
+      outputChange = "../test-cases/" + id + "/output-change.nq";
+      outputChangeStr = await fs.promises.readFile(outputChange, 'utf-8')
+      outputChangeStr = outputChangeStr.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      console.log(`${id}: output change is read from file`);
+
+      const rulesBase = "../test-cases/" + id + "/base.rml.ttl"
+      let rulesBaseStr = await fs.promises.readFile(rulesBase, 'utf-8');
+      rulesBaseStr = rulesBaseStr.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      console.log(`${id}: base rules are read from file`);
+
+      const rulesChange = "../test-cases/" + id + "/change.rml.ttl"
+      let rulesChangeStr = await fs.promises.readFile(rulesChange, 'utf-8');
+      rulesChangeStr = rulesChangeStr.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      console.log(`${id}: change rules are read from file`);
 
       let specRef = null;
       if (data.has('specRef')) {
         specRef = data.get('specRef').value || null;
       }
 
-      testcases.push({
-        iri: data.get('s').value,
-        title: data.get('name').value,
-        description: data.get('description').value,
-        rules,
-        rulesStr,
-        id,
-        errorExpected: '' + (data.get('expectedResult').value === 'http://rml.io/ns/test-case/InvalidRulesError'),
-        output,
-        outputStr,
-        specRef
-      });
+      if (processed.indexOf(data.get('s').value) == -1) {
+        testcases.push({
+          iri: data.get('s').value,
+          title: data.get('name').value,
+          description: data.get('description').value,
+          rulesBase,
+          rulesBaseStr,
+          rulesChange,
+          rulesChangeStr,
+          id,
+          errorExpected: false,
+          outputBase,
+          outputBaseStr,
+          outputChange,
+          outputChangeStr,
+          specRef
+        });
+        processed.push(data.get('s').value);
+      }
 
       console.log(`${id}: done.`);
     } else {
